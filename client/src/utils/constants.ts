7af993c677cef4ccf3079e9281d4ed89317eb976
hevelius <perrotta.mr@gmail.com>
2022-11-04 17:11:05 +0100

    chore: add config

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,8 @@
-import { HelloWorld__factory as HelloWorld } from "backend/typechain-types";
+import { NFTMarket__factory } from "backend/typechain-types";
+import { NFT__factory } from "backend/typechain-types";
 
-export const contractAbi = HelloWorld.abi;
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftMarketAbi = NFTMarket__factory.abi;
+export const nftMarketAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const nftAbi = NFT__factory.abi;
+export const nftAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
