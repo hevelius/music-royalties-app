49eba912793b248109738120d286166885eabd7e
hevelius <perrotta.mr@gmail.com>
2022-12-29 14:34:21 +0100

    feat: add gas reporter

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
@@ -7,7 +8,11 @@ const config: HardhatUserConfig = {
     hardhat: {
       allowUnlimitedContractSize: true,
     }
-  }
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD"
+  }  
 };
 
 export default config;
